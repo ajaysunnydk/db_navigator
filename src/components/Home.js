702f090e007b55b4d78edc22913d3e9ae5b57357
ajaysunnydk <ajaysunnydk167@gmail.com>
2023-02-11 15:06:04 +0530

    updated list style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import './style.css'
 
 const Home = () => {
     debugger;
@@ -12,50 +13,33 @@ const Home = () => {
         'withCredentials': true
     };
     console.log(reqBody)
-    const [topics,setTopics] = useState([]);
-    axios.post("http://localhost:8080/dextrus/",reqBody,{ headers: headers, cache: false })
-    .then(resp=>{
-        setTopics(resp.data)
-        console.log(resp.data)
-    }).catch(error=>{
-        console.log("catch")
-        console.log(error)
-    })
+    const [topics, setTopics] = useState([]);
+    axios.post("http://localhost:8080/dextrus/", reqBody, { headers: headers, cache: false })
+        .then(resp => {
+            setTopics(resp.data)
+            console.log(resp.data)
+        }).catch(error => {
+            console.log("catch")
+            console.log(error)
+        })
 
-    
 
-    const [expandedTopics, setExpandedTopics] = useState([]);
-
-    const toggleExpand = topicTitle => {
-        const currentIndex = expandedTopics.indexOf(topicTitle);
-        const newExpandedTopics = [...expandedTopics];
-
-        if (currentIndex === -1) {
-            newExpandedTopics.push(topicTitle);
-        } else {
-            newExpandedTopics.splice(currentIndex, 1);
-        }
-
-        setExpandedTopics(newExpandedTopics);
-    };
+    const toggleExpand = (topic) => {
+        console.log(topic + " Clicked")
+    }
 
     return (
-        <ul>
+        <div className="left-nav">
+        <div className="catalogs-list" >
             {topics.map(topic => (
-                <li key={topic}>
-                    <button onClick={() => toggleExpand(topic)}>
-                        {topic}
-                    </button>
-                    {expandedTopics.includes(topic) && (
-                        <ul>
-                            {topic.subTopics.map(subTopic => (
-                                <li key={subTopic.id}>{subTopic.title}</li>
-                            ))}
-                        </ul>
-                    )}
-                </li>
+                <button onClick={() => toggleExpand(topic)}>
+                    {topic}
+                </button>
             ))}
-        </ul>
+        </div>
+        </div>
+
+
     );
 };
 
